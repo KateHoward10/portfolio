b4a7b2244b815a00c21c614b0fc3b273810f9ed4
Kate Howard <k.l.howard.1992@gmail.com>
2019-10-01 21:39:53 +0100

    Dark mode toggle

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql, useStaticQuery } from "gatsby"
-import { Container, TextContainer, Image } from "./styles"
+import {
+	Container,
+	TextContainer,
+	Image,
+	ToggleContainer,
+	Toggle,
+	Hidden,
+	Slider,
+} from "./styles"
 import me from "../../images/picture-of-me.jpg"
 
 const Header = () => {
+	const [darkMode, setDarkMode] = useState(false)
 	const data = useStaticQuery(graphql`
 		query {
 			site {
@@ -24,6 +33,21 @@ const Header = () => {
 					and React Native. Here is some of my work.
 				</h3>
 			</TextContainer>
+			<ToggleContainer>
+				<span role="img" aria-label="sun">
+					🌞
+				</span>
+				<Toggle darkMode={darkMode}>
+					<Hidden
+						type="checkbox"
+						onChange={e => setDarkMode(e.target.checked)}
+					/>
+					<Slider darkMode={darkMode} />
+				</Toggle>
+				<span role="img" aria-label="moon">
+					🌙
+				</span>
+			</ToggleContainer>
 		</Container>
 	)
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -27,3 +27,38 @@ export const Image = styled.img`
 	border-radius: 50%;
 	margin: 20px;
 `
+
+export const ToggleContainer = styled.div`
+	position: absolute;
+	top: 5px;
+	right: 10px;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+`
+
+export const Toggle = styled.label`
+	width: 60px;
+	height: 36px;
+	border-radius: 18px;
+	background-color: lightgrey;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	margin: 5px;
+	justify-content: ${props => (props.darkMode ? "flex-end" : "flex-start")};
+`
+
+export const Hidden = styled.input`
+	display: none;
+`
+
+export const Slider = styled.div`
+	cursor: pointer;
+	width: 30px;
+	height: 30px;
+	border-radius: 50%;
+	margin: 3px;
+	box-shadow: 0 0 6px grey;
+	background-color: ${props => (props.darkMode ? "navy" : "deepskyblue")};
+`
