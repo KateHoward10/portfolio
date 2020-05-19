f12804681d0c609a491357abfb5bda2da90d8600
Kate Howard <k.l.howard.1992@gmail.com>
2020-05-20 00:25:49 +0100

    Update Info background

--- src/components/Info/styles.js
+++ src/components/Info/styles.js
@@ -1,16 +1,24 @@
 import styled from "styled-components"
 
 export const Box = styled.div`
-  box-shadow: 0 0 10px grey;
-  border-radius: 5px;
-  margin: 4vw 8vw;
+  max-width: 100vw;
   padding: 20px;
-  max-width: 920px;
+  padding-top: 20%;
+  background: linear-gradient(32deg, hsla(210, 100%, 70%, 10%) 55%, hsla(220, 100%, 70%, 15%) 45%), linear-gradient(154deg, transparent 70%, hsla(210, 100%, 70%, 15%) 30%);
+  clip-path: polygon(0 6%, 100% 0, 100% 100%, 0% 100%);
+	@media screen and (min-width: 420px) {
+    padding-top: 30px;
+    background: linear-gradient(9deg, hsla(210, 100%, 70%, 10%) 50%, hsla(220, 100%, 70%, 15%) 40%), linear-gradient(174deg, transparent 70%, hsla(210, 100%, 70%, 15%) 30%);
+    clip-path: polygon(0 12%, 100% 0, 100% 100%, 0% 100%);
+  }
 `
 
 export const List = styled.ul`
+  max-width: 920px;
   list-style: none;
   padding: 0;
+  margin: 4vw auto;
+  background-color: transparent;
 `
 
 export const ListItem = styled.li`
